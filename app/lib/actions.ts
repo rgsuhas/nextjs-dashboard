97560354c8c3b516d9f4e324fa97d706323c9a9b
rgsuhas <rgsuhas6364@gamil.com>
2025-07-20 02:48:07 +0530

    feat: db connected to insert form data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { z } from 'zod';
+import postgres from 'postgres';
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
 const FormSchema = z.object({
   id: z.string(),
@@ -13,10 +16,17 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);}
+  });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+     await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+}// 
