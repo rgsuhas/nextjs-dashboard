b557e3ffbe7ee2105a67eb466a21d4c62063bf00
rgsuhas <rgsuhas6364@gamil.com>
2025-07-20 01:57:30 +0530

    search and pagination

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
+// This component is used to show a loading state while the dashboard data is being fetched.
